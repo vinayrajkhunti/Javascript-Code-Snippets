1ca74e7ab15794e512d5df8b35b1bf3d9f83dc8c
vinayraj khunti <vinayrajkhunti@users.noreply.github.com>
2016-04-18 12:36:54 -0700

    Update removeDuplicate.js

--- For-Loop-If-else/removeDuplicate.js
+++ For-Loop-If-else/removeDuplicate.js
@@ -1,1 +1,37 @@
 
+
+
+
+/*THIS EXAMPLE DEMONSTRATE USE OF NESTED FOR LOOP AND IF-ELSE.
+IT WILL COMPARE THE ARRAY VALUES, AND IF THERE IS ANY DUPLICATION, 
+IT WILL REMOVE IT AND CONSOLES THE ARRAY WITHOUT DUPLICATE VALUES.*/
+
+
+
+var i;
+var j;
+
+
+function compareNames()
+{
+	
+	var name = ["sam", "smith", "tanmay", "sam", "utkarsh", "vinay", "tanmay"];
+	var x = name.sort();
+
+	for(i = 0; i <= name.length - 1; i++)
+	{
+		for(j = i + 1; j <= name.length - 1; j++)
+		{
+			if(name[i]==name[j])
+			{
+				for(k = j; k < x.length - 1; k++)
+				{
+					x[k] = x[k + 1];
+				}
+					x.pop();
+				 
+			}
+		}
+	}
+console.log(x);
+}
