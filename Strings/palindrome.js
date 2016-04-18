b427e41faf1670c22639a439b35cf3a33dabb4d0
vinayraj khunti <vinayrajkhunti@users.noreply.github.com>
2016-04-18 12:52:56 -0700

    Update palindrome.js

--- Strings/palindrome.js
+++ Strings/palindrome.js
@@ -1,1 +1,23 @@
+/* THIS IS AN EXAMPLE OF USE OF STRING. 
+IT CHECKS WETHER NUMBER ENTERED IN A PALINDROM OR NOT,
+AND DISPLAYS MESSAGE ACCORDINGLY. */
 
+function enterPalindrome()
+{
+	var x = prompt('Enter any string');
+	var x = x.toLowerCase();
+
+	for(var i = 0; i <= x.length; i++)
+
+		if(x.charAt(i) == x.charAt(x.length - 1) && x.charAt(i + 1) == x.charAt(x.length - 2) )
+		{
+			alert('Yes It Is Palindrome.');
+			break;
+		}
+		else
+		{
+			alert('No Its Not A Palindrome.');
+			break;
+		}
+	
+}
