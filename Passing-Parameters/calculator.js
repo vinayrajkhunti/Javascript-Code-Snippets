512a1450936605303c98e5dff4769396f06207e5
vinayraj khunti <vinayrajkhunti@users.noreply.github.com>
2016-04-18 12:48:20 -0700

    Update calculator.js

--- Passing-Parameters/calculator.js
+++ Passing-Parameters/calculator.js
@@ -1,1 +1,37 @@
+/* THIS IS EXAMPLE OF SIMPLE CALCULATOR USING "passing parameter" FEATURE OF JAVASCRIPT.
+I HAVE USED "SWITHC-CASE" FUNCTIONALITY HERE.*/
+
+
+function calc(b)
+{
+	var x = document.getElementById('t1').value;
+	 x = parseInt(x);
+	var y = document.getElementById('t2').value;
+	 y = parseInt(y);
+
+	switch(b)
+	{
+		case "+" :
+		document.getElementById('t3').value =  x + y;
+		break;
+
+		case "-" :
+		document.getElementById('t3').value = x - y;
+		break;
+
+		case "*" :
+		document.getElementById('t3').value = x * y;
+		break;
+
+		case "+" :
+		
+		document.getElementById('t3').value = x / y;
+		break;
+
+		default:
+			alert('hello');
+
+    }
+}
+
 
